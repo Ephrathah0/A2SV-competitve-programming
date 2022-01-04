0367d5b55b7fd86c6f61995c730ba5624dd60f89
Ephrathah0 <85620950+Ephrathah0@users.noreply.github.com>
2022-01-04 09:44:16 +0300

    Update if-else.py

--- python practice/if-else.py
+++ python practice/if-else.py
@@ -1,1 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n%2 != 0:
+    print("Weird")
+elif n%2==0 and range(2,5):
+    print("Not Weird")
+elif n%2 and range(6,20):
+    print("Weird")
+else:
+     print("Not Weird")  
 
