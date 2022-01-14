e927d244d28cd5afc8317933969ff568458b7539
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-01-14 14:11:15 +0300

    Create zipped.py

---
+++ python practice/zipped.py
@@ -0,0 +1,10 @@
+n, x = map(int, input().split())
+marks = list()
+for i in range(x):
+    marks.append(input().split())
+marks = zip(*marks)
+for i in marks:
+    sum = 0
+    for j in range(x):
+        sum += float(i[j])
+    print(sum / x)
