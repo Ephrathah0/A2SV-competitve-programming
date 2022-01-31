557d432f9422ece5573f8b3375de8f1dcdb7cda8
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-01-31 18:26:02 +0300

    Create counting sort.py

---
+++ week 7/counting sort.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def countingSort(arr):
+    countarray= [0] * 100
+    for num in arr:
+        countarray[num] += 1
+    return countarray   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
