8810d9c3c32b453f389a5c564e2136569a253ed8
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-02-01 18:13:24 +0300

    Create reverse linked list.py

---
+++ week 7/reverse linked list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr=head
+        while curr:
+            next_head = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_head
+        return prev
+            
+        
