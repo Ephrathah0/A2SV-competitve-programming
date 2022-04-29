1ff5f10302c79109e5fe6a7e1f5dc87c6fb35408
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-04-29 18:36:34 +0300

    Create implement trie.py

---
+++ After camp/week 1/implement trie.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+           
+    def addWord(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.isWord = True        
+    
+    def search(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+        if current.isWord:
+            return True
+        else:
+            return False
+    
+    def startsWith(self, prefix):
+        current = self.root
+        for char in prefix:
+            if char in current:
+                current = current.children[char]
+            else:
+                return False
+        return True
